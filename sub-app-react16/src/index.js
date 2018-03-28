f446bd90db2f23402f642246c2476ce23bbcea4e
mlip <kiwaczki@gmail.com>
2018-03-28 11:53:27 +0200

    adding single spa callbacks to be added in webpack build

--- sub-app-react16/src/index.js
+++ sub-app-react16/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {bootstrap, mount, unmount} from './react.app'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
