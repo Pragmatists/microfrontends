7f15ca63ad1277c2509551806fb833e6cca65132
Artur Chyży <artur.chyzy@gmail.com>
2018-03-28 10:27:34 +0200

    User details from session storage

--- sub-app-angular/src/app/app.component.ts
+++ sub-app-angular/src/app/app.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sub-app-angular';
-  user = {
+  user;
+  fakeUser = {
     username: 'etest@example.com',
     displayName: 'Test Test'
   };
+
+  ngOnInit(): void {
+    // sessionStorage.setItem('USER', JSON.stringify({
+    //   username: 'session@example.com',
+    //   displayName: 'Test Session'
+    // }));
+    this.user = JSON.parse(sessionStorage.getItem('USER') || JSON.stringify(this.fakeUser));
+  }
 
 }
