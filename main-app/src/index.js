abdd4c9ec4e2217c928e9181ed8c2511c8e5c380
Artur Chyży <artur.chyzy@gmail.com>
2018-03-28 12:33:36 +0200

    Hinclude try - not working as hinclude does not support scripts with src (only inline scripts)

--- main-app/src/index.js
+++ main-app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import hinclude from '../node_modules/Hinclude/hinclude';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
