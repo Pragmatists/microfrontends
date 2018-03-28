60cb31f2b8d1c39ca25a2d7336f8bc01feb2c022
mlip <kiwaczki@gmail.com>
2018-03-28 15:35:11 +0200

    remove bootstrap from main app

--- main-app/src/App.js
+++ main-app/src/App.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Button} from "react-bootstrap";
-import './styles/bootstrap/bootstrap.min.css';
 
 class App extends Component {
     render() {
@@ -13,7 +11,6 @@ class App extends Component {
                     <h1 className="App-title">Welcome to React 15</h1>
                 </header>
                 <div className="App-intro">
-                    <Button bsStyle="primary">Bootstrap 3 button</Button>
                     <a href="/react">Go to React 16</a><br/>
                     <a href="/angular">Go to Angular</a><br/>
                     <div style={{borderStyle: "dashed"}} id="react-app"></div>
