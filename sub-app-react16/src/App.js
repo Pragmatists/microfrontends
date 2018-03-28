5c346fbba34f2a00e0e709d53aa71ca8a627939a
mlip <kiwaczki@gmail.com>
2018-03-28 10:57:25 +0200

    react 16 part

---
+++ sub-app-react16/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {ProjectsList} from "./ProjectsList";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ProjectsList/>
+      </div>
+    );
+  }
+}
+
+export default App;
